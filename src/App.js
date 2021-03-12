6c2d52218022b4fe8d91544624554c4637c69e13
Sebastian Asis <sasisromero10@gmail.com>
2021-03-11 19:08:14 -0500

    :fire: NEW: Navbar implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import { Home } from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+          <Switch>
+            <Route exact path = "/" component={Home}/>
+          </Switch>
+      </Router>
+    </>
   );
 }
 
