b41ac4eb8e7025433e170173a47a0c0a61310fab
Sebastian Asis <sasisromero10@gmail.com>
2021-03-12 12:12:37 -0500

    :dart: IMPROVE: Cards

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../App.css';
-// import Cards from '../Cards';
+import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 // import Footer from '../Footer';
 
@@ -8,8 +8,8 @@ export function Home() {
   return (
     <>
       <HeroSection />
-      {/* <Cards />
-      <Footer /> */}
+      <Cards />
+      {/* <Footer /> */}
     </>
   );
 }
